63419f591ee4f87273d7ba0cfaa40c1aa7ed05ab
Volodymyr Zahorodnyy <volodymyr@Volodymyrs-MBP.hitronhub.home>
2020-06-23 13:29:06 -0700

    Updates

---
+++ precache-manifest.f20f41d68aef2443d2208ad2e8f0b138.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508159302da660fd8578731344687563",
+    "url": "/deploy-react-app/index.html"
+  },
+  {
+    "revision": "3eb665422047007dfe0b",
+    "url": "/deploy-react-app/static/css/main.576be4c1.chunk.css"
+  },
+  {
+    "revision": "b112160e34c87489bb7a",
+    "url": "/deploy-react-app/static/js/2.de57095c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react-app/static/js/2.de57095c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb665422047007dfe0b",
+    "url": "/deploy-react-app/static/js/main.16ab346c.chunk.js"
+  },
+  {
+    "revision": "28a9bb8101bfcf797a06",
+    "url": "/deploy-react-app/static/js/runtime-main.842bdf1e.js"
+  },
+  {
+    "revision": "ab57ea18497f53bd1b66336cfa9b8e6d",
+    "url": "/deploy-react-app/static/media/BooksLogo.ab57ea18.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
