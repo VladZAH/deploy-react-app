6104f9dc1488f99143b78080742082f0728e1080
VladZAH <54724049+VladZAH@users.noreply.github.com>
2020-11-08 18:12:38 -0800

    Add files via upload
    Added 'src' folder to see all the source code, and 'package.json' to see all the dependencies.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BookSearchForm from './components/BookSearchForm'
+
+
+const App = () => {
+  
+  return (
+    <div>
+      <BookSearchForm />
+    </div>
+  );
+}
+
+export default App;
